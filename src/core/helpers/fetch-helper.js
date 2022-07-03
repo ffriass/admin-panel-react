471b7c1892e5be62930d8b8023af966cc98fa387
Franklin Frias <fa.frias@outlook.com>
2022-07-03 19:15:21 -0400

    simplify shared fetch method

--- src/core/helpers/fetch-helper.js
+++ src/core/helpers/fetch-helper.js
@@ -1,14 +1,7 @@
-import isFunction from 'lodash/isFunction';
 import has from 'lodash/has';
 import get from 'lodash/get';
 import isObject from 'lodash/isObject';
-import isNil from 'lodash/isNil';
 import pick from 'lodash/pick'
-
-let onHandle401Request;
-let onHandleStatus;
-
-let globalModifyHeader = null;
 
 const allowedFetchOptions = [
   'method',
@@ -32,86 +25,18 @@ const allowedFetchOptions = [
  * @param {boolean} didThrowException
  */
 
-const _internalFetch = (url, options/*, dispatch, getState*/) => {
-  let didCallDispatch = false
-  // TODO: adjust later
-  /*const innerDispatch = (body, success, response, didThrowException) =>{
-    const dispatchType = success
-      ? `${options.key}_FULFILLED`
-      : `${options.key}_ERROR`;
-  
-    if (body) {
-      if (options.convertToCamelCase) {
-        body = snakeToCamelCaseObject(body);
-      } else if (options.convertToSnakeCase) {
-        body = camelToSnakeCaseObject(body);
-      }
-    }
-    const name = !success ? 'error' : '';
-  
-    didCallDispatch = true;
-    try {
-      dispatch({
-        type: dispatchType,
-        body: body,
-        name: name,
-        extraArguments: options.extraArguments,
-        response,
-      });
-    } catch (ex) {
-      console.log(`exception in reducer ${dispatchType}: ${ex.message}`);
-      //TODO: do not throw yet
-      //throw ex
-    }
-  
-    if (onHandleStatus && !options.skipStatusCallback) {
-      onHandleStatus(response.status, body);
-    }
-  
-    if (!success) {
-      if (
-        !options.dontShowErrorsInNotificationBar &&
-        body.message &&
-        !didThrowException
-      ) {
-        console.error(`Call failed from server: ${options.key}: ${body.message}`);
-        console.error('url: ' + options.url);
-        console.error(body);
-  
-        if (window) {
-          if (response.status !== 402) {
-            // Don't show for Payment error status 402
-            // Pass the message to the notification bar to show the error message
-            window.postMessage(
-              {
-                type: 'fetch_notification',
-                message: body.message,
-                persistent: body.persistent,
-              },
-              window.document.origin
-            );
-          }
-        }
-      }
-  
-      if (options.onError) {
-        options.onError(body, dispatch, getState);
-      }
-    }
-  };
+const _internalFetch = (url, options) => {
 
-  dispatch({
+  /*dispatch({
     type: `${options.key}_PENDING`,
     extraArguments: options.extraArguments,
   });*/
 
   const request = new Request(url, {
-    ...options.fetchOptions,
+    ...pick(options.fetchOptions, allowedFetchOptions),
     url,
     skipCredentials: false,
   });
-
-  //const request = new Request(url, pick(options.fetchOptions, allowedFetchOptions));
 
   let responsePromise = fetch(request).then((response) => response /*_checkAuthentication(response, url, options)*/);
 
@@ -120,34 +45,25 @@ const _internalFetch = (url, options/*, dispatch, getState*/) => {
     return res.text().then((data) => ({ response: res, data: data }));
   });
 
-  //Not applocable for the moment
-  /*if (options.appendPromise) {
-    responsePromise = responsePromise.then(options.appendPromise)
-  }*/
-
   return responsePromise
     .then((result) => {
-      console.log('Result: ', result);
+      
       const obj = {
         response: result.response,
         payload: result.data,
       };      
-
+    
       // Check if returnType is defined, if not, skip the 'parsing' and fallback to text
-      if (
-        typeof options.returnType === 'undefined' ||
-        options.returnType === null ||
-        options.returnType === ''
-      ) {
+      if (!options.returnType)
         return obj;
-      }
+      
 
       // Convert text to the return type (only JSON supported for now)
       if (options.returnType === 'json') {
         if (obj.payload.length > 0) {
           obj.payload = JSON.parse(obj.payload);
 
-          //TODO: ad feature. Not suported yet
+          //TODO: add feature. Not suported yet
           /*if (options.convertToCamelCase) {
             obj.payload = snakeToCamelCaseObject(obj.payload)
           } else if (options.convertToSnakeCase) {
@@ -162,6 +78,7 @@ const _internalFetch = (url, options/*, dispatch, getState*/) => {
           `Returntype ${options.returnType} not supported, fallback to text`
         );
       }
+      console.log('Result 1: ', obj);
 
       return obj;
     })
@@ -175,217 +92,39 @@ const _internalFetch = (url, options/*, dispatch, getState*/) => {
           ? JSON.stringify(result.payload)
           : result.payload;
 
-        throw new Error(errorMessage);
+          console.log('Result 2: ', !errorMessage ? result.response.statusText : errorMessage);
+
+        throw new Error(!errorMessage ? result.response.statusText : errorMessage);
       } else {
         //innerDispatch(result.payload, true, result.response);
 
         return {
-          payload: result.payload//,
-          //getState,
+          payload: result.payload,          
+          isSuccessful: true
         };
       }
     })
     .catch((error) => {
-      console.log('Error: ', error);
-      // Firefox throws a TypeError when navigating to a page when a request is incomplete
-      // we dont want to throw a message when this happends.
-      // https://github.com/github/fetch/issues/310
-      // https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Global_Objects/TypeError
-      const dontLog = error.constructor === TypeError;
-
-      //TODO: Create retry feature when applicable
-      // cdn url retry with non cdn url
-      /*if (isKnownCdnUrl(url)) {
-        const nonCdnUrl = convertSiteNameToNonCdnUrl(url)
-        _internalFetch(nonCdnUrl, options, dispatch, getState)
-      }*/
-
-      /*if (options.onException) {
-        options.onException(error, dispatch, getState);
-      } else if (!didCallDispatch) {
-        // if we abort the request ourselves we have AbortError in the 'name'
-        // property of the error. Dont dispatch anything, just cancel the
-        // request
-        if (error.name !== 'AbortError') {
-          // innerDispatch({ message: 'Oops...something went wrong' }, false, {}, dontLog);
-        }
-      } else {
-        const errorObjWithDispatch = {
-          error: error,
-          dispatch,
-          getState,
-        };
-        throw errorObjWithDispatch;
-      }*/
-
+      //TODO: Create retry feature when applicable calling _internalFetch again with adjustemnts
+      
       return {
         error: error,
+        isSuccessful: false
       };
     });
 };
 
-/*const _innerDispatch = (body, success, response, didThrowException, didCallDispatch = false) => {
-  const dispatchType = success
-    ? `${options.key}_FULFILLED`
-    : `${options.key}_ERROR`;
-
-  if (body) {
-    if (options.convertToCamelCase) {
-      body = snakeToCamelCaseObject(body);
-    } else if (options.convertToSnakeCase) {
-      body = camelToSnakeCaseObject(body);
-    }
-  }
-  const name = !success ? 'error' : '';
-
-  didCallDispatch = true;
-  try {
-    dispatch({
-      type: dispatchType,
-      body: body,
-      name: name,
-      extraArguments: options.extraArguments,
-      response,
-    });
-  } catch (ex) {
-    console.log(`exception in reducer ${dispatchType}: ${ex.message}`);
-    //TODO: do not throw yet
-    //throw ex
-  }
-
-  if (onHandleStatus && !options.skipStatusCallback) {
-    onHandleStatus(response.status, body);
-  }
-
-  if (!success) {
-    if (
-      !options.dontShowErrorsInNotificationBar &&
-      body.message &&
-      !didThrowException
-    ) {
-      console.error(`Call failed from server: ${options.key}: ${body.message}`);
-      console.error('url: ' + options.url);
-      console.error(body);
-
-      if (window) {
-        if (response.status !== 402) {
-          // Don't show for Payment error status 402
-          // Pass the message to the notification bar to show the error message
-          window.postMessage(
-            {
-              type: 'fetch_notification',
-              message: body.message,
-              persistent: body.persistent,
-            },
-            window.document.origin
-          );
-        }
-      }
-    }
-
-    if (options.onError) {
-      options.onError(body, dispatch, getState);
-    }
-  }
-};*/
-
-const deleteAsJson = (data, options, headers) => {
-  const obj = {
-    returnType: 'json',
-    ...options,
-  };
-
-  obj.fetchOptions = {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      ...obj.fetchOptions.headers,
-    },
-    body: JSON.stringify(data),
-    extraArguments: options.extraArguments,
-  };
-
-  return fetchAsJson(obj);
-};
-
-/**
- * put json to the given url
- *
- * @function
- * @param {string} url - string
- * @param {object} data, will be send as json
- * @param {object} options, object.key is required.
- * You can add object.extraArguments, these will be available in the reducer when the fetch is completed
- * @returns promise, that will dispatch redux events
- *
- * @memberOf FetchHelper
- */
-const putAsJson = (data, options, headers) => {
-  const obj = {
-    returnType: 'json',
-    ...options,
-  };
-  obj.fetchOptions = {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      ...obj.fetchOptions.headers,
-    },
-    body: JSON.stringify(data),
-  };
-
-  return fetchAsJson(obj);
-};
-
-/**
- * post json to the given url
- *
- * @function
- * @param {string} url - string
- * @param {object} data, will be send as json
- * @param {object} options, object.key is required.
- * You can add object.extraArguments, these will be available in the reducer when the fetch is completed
- * @returns promise, that will dispatch redux events
- *
- * @memberOf FetchHelper
- */
-const postAsJson = (data, options, headers) => {
-  const obj = {
-    returnType: 'json',
-    ...options,
-  };
-
-  obj.fetchOptions = {
-    ...obj.fetchOptions,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      ...obj.fetchOptions.headers,
-    },
-    body: JSON.stringify(data),
-  };
-  return fetchAsJson(obj);
-};
-
-const fetchAsJson = (options) => {
+const xFetch = (options) => {
   if (!options || !options.key) {
     throw new Error('Missing option.key Attribute');
   }
 
-  /*if (options.fetchOptions && options.fetchOptions.method && options.fetchOptions.method !== 'GET') {
-    clearCache()
-  }*/
-  return _internalFetch(options.url, options);
+  //TODO: start using cache when nedeed ang clear it when is not GET
 
-  /*return function (dispatch, getState) {
-    return _internalFetch(options.url, options, dispatch, getState);
-  };*/
+  return _internalFetch(options.url, options);
 };
 
 
 export {
-  fetchAsJson,
-  putAsJson,
-  postAsJson,
-  deleteAsJson
+  xFetch
 }
--- src/core/helpers/fetch-options-helper.js
+++ src/core/helpers/fetch-options-helper.js
@@ -1,11 +1,11 @@
 //import { getUrl, siteNames } from '@eig-builder/core-utils/helpers/url-helper';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pdXN1YXJpb0BiYXJiZXJob21lLmNvbSIsIm5hbWVpZCI6IjQiLCJyb2xlIjoiQ3VzdG9tZXIiLCJOYW1lIjoiRnJhbmtsaW4iLCJMYXN0TmFtZSI6IkZyaWFzIiwiUGhvbmVOdW1iZXIiOiI4MDktODg4LTg4ODgiLCJ1c2VyVmVyaWZpZWQiOiJUcnVlIiwiQmlydGhEYXRlIjoiIiwiaXNUZW1QYXNzd29yZCI6IkZhbHNlIiwiaXNOZXdVc2VyIjoiRmFsc2UiLCJwYXltZW50TWV0aG9kcyI6IkNBU0gsUEFZUEFMIiwibmJmIjoxNjU2NzI1ODI0LCJleHAiOjE2NTY4MTIyMjQsImlhdCI6MTY1NjcyNTgyNCwiaXNzIjoiQmFyYmVySG9tZS5jb20iLCJhdWQiOiJCYXJiZXJIb21lLmNvbSJ9.4OBRaM-YMJX3eHdbA546pxTqWZw-Fe1RyRnGStC2feM';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pdXN1YXJpb0BiYXJiZXJob21lLmNvbSIsIm5hbWVpZCI6IjQiLCJyb2xlIjoiQ3VzdG9tZXIiLCJOYW1lIjoiRnJhbmtsaW4iLCJMYXN0TmFtZSI6IkZyaWFzIiwiUGhvbmVOdW1iZXIiOiI4MDktODg4LTg4ODgiLCJ1c2VyVmVyaWZpZWQiOiJUcnVlIiwiQmlydGhEYXRlIjoiIiwiaXNUZW1QYXNzd29yZCI6IkZhbHNlIiwiaXNOZXdVc2VyIjoiRmFsc2UiLCJwYXltZW50TWV0aG9kcyI6IkNBU0gsUEFZUEFMIiwibmJmIjoxNjU2ODY5OTAwLCJleHAiOjE2NTY5NTYzMDAsImlhdCI6MTY1Njg2OTkwMCwiaXNzIjoiQmFyYmVySG9tZS5jb20iLCJhdWQiOiJCYXJiZXJIb21lLmNvbSJ9.KHWDJ8JHF54jDVhqU9aV8HG4nGONFPd4e_0spRJShXE';
 
-export const getApiOptions = (endpoint, key, returnType = 'json', useCache = true) => {
+export const getApiOptions = (endpoint, key, returnType = 'json', useCache = false) => {
   const options = {
     key,
-    //TODO:createmethod to get the base URL
+    //TODO:create method to get the base URL
     url: `https://localhost:5001/${endpoint}`, //getUrl(siteNames.API_CRM, endpoint),
     returnType,
     useCache,
@@ -17,6 +17,44 @@ export const getApiOptions = (endpoint, key, returnType = 'json', useCache = tru
       }
     }    
   };
+
+  return options;
+};
+
+export const getPostApiOptions = (endpoint, data, key, returnType = 'json', useCache = false) => {
+
+  let options = getApiOptions(endpoint, key, returnType, useCache)
+
+  const obj = {
+    returnType: 'json',
+    ...options,
+  };
+
+  obj.fetchOptions = {
+    ...obj.fetchOptions,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...obj.fetchOptions.headers,
+    },
+    body: JSON.stringify(data),
+  };
+
+  return options;
+};
+
+export const getPutApiOptions = (endpoint, data, key, returnType = 'json', useCache = false) => {
+
+  let options = getPostApiOptions(endpoint, data, key, returnType, useCache)
+  options.fetchOptions.method = 'PUT';
+
+  return options;
+};
+
+export const getDeleteApiOptions = (endpoint, data, key, returnType = 'json', useCache = false) => {
+
+  let options = getPostApiOptions(endpoint, data, key, returnType, useCache)
+  options.fetchOptions.method = 'DELETE';
 
   return options;
 };
